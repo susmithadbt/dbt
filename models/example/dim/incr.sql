088ce214e809ef2580293a9876ff998dc409f021
reachtosusmitha96@gmail.com <reachtosusmitha96@gmail.com>
2023-07-09 16:51:52 +0000

    commit

---
+++ models/example/dim/incr.sql
@@ -0,0 +1,9 @@
+{{ config(
+    materialized = 'incremental'
+) }}
+      with orders as(
+      select * from {{ref(stg_orderdata)}}
+      {% if is_incremental()%}
+      where order_date >=(select max(order_date)from {{this}})
+      {%endif%}
+)
--- models/example/src/samporder.sql
+++
@@ -1,5 +0,0 @@
-{{config(
-    materialized="incremental"
-    full_refresh=true
-)}}
-select * from DBT_DATABASE.PUBLIC.ORDERS
